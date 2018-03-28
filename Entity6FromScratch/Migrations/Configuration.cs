f580e51218dfcb650baa60528bc7677204d3401f
gorohoroh <gorohoroh@gmail.com>
2018-03-28 16:05:57 +0300

    Invoked enable-migrations

---
+++ Entity6FromScratch/Migrations/Configuration.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+
+namespace Entity6FromScratch.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<Entity6FromScratch.ProductContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+    } 
+}
