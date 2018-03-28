bc6e37dcc39c0bd58f4f3384df1c2fd0017b52f7
gorohoroh <gorohoroh@gmail.com>
2018-03-28 20:09:31 +0300

    Model update and an add-migration on top of it

---
+++ Entity6FromScratch/Migrations/201803281704325_IsCompanyPublic.cs
@@ -0,0 +1,18 @@
+﻿namespace Entity6FromScratch.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class IsCompanyPublic : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Vendors", "IdPublicCompany", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Vendors", "IdPublicCompany");
+        }
+    }
+}
